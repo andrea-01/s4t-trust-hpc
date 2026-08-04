36c730cfd1c726bdc3326d8c7e7ee2b3b59b48c0
adima <adimarco102@gmail.com>
2026-08-04 12:29:33 +0200

    feat(deploy): docker-compose per testnet a 3 container e config network

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    docker: {
+      url: process.env.RPC_URL || "http://hardhat-node:8545"
+    }
+  }
 };
 
 export default config;
